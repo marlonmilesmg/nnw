2a2b7e40ced130540eb0f338fbdd72595bdafdd5
marlonmilesmg <magonjomarlon@gmail.com>
2024-04-22 09:16:09 +0200

    inception

---
+++ src/main/java/za/co/marlonmagonjo/nnw/NnwApplication.java
@@ -0,0 +1,13 @@
+package za.co.marlonmagonjo.nnw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NnwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/co/marlonmagonjo/nnw/NnwApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.marlonmagonjo.nnw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
