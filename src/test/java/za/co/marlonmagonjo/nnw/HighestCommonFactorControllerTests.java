447b29842d784c527e75e16e2dc1bfd8040e492f
marlonmilesmg <magonjomarlon@gmail.com>
2024-04-24 11:13:35 +0200

    updated HighestCommonFactorControllerTests

--- src/test/java/za/co/marlonmagonjo/nnw/HighestCommonFactorControllerTests.java
+++ src/test/java/za/co/marlonmagonjo/nnw/HighestCommonFactorControllerTests.java
@@ -1,6 +1,7 @@
 package za.co.marlonmagonjo.nnw;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import za.co.marlonmagonjo.nnw.controller.HighestCommonFactorController;
 import za.co.marlonmagonjo.nnw.model.NumbersRequest;
@@ -12,17 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class HighestCommonFactorControllerTests {
 
     @Test
-    public void test_returns_correct_hcf_for_valid_input() {
-        HighestCommonFactorService hcfService = new HighestCommonFactorService();
-        HighestCommonFactorController hcfController = new HighestCommonFactorController(hcfService);
-        NumbersRequest numbersRequest = new NumbersRequest(new int[]{2, 4, 6});
-
-        ResponseEntity<Integer> result = hcfController.highestCommonFactor(numbersRequest);
-
-        assertEquals(2, result.getBody().intValue());
-    }
-
-    @Test
     public void test_throws_exception_when_input_array_is_null() {
         HighestCommonFactorService hcfService = new HighestCommonFactorService();
         HighestCommonFactorController hcfController = new HighestCommonFactorController(hcfService);
@@ -31,5 +21,53 @@ public class HighestCommonFactorControllerTests {
         assertThrows(NullPointerException.class, () -> {
             hcfController.highestCommonFactor(numbersRequest);
         });
+    }
+
+    @Test
+    public void test_valid_array_of_positive_integers() {
+        HighestCommonFactorService hcfService = new HighestCommonFactorService();
+        HighestCommonFactorController hcfController = new HighestCommonFactorController(hcfService);
+        NumbersRequest numbersRequest = new NumbersRequest(new int[]{2, 4, 6, 8});
+
+        ResponseEntity<?> response = hcfController.highestCommonFactor(numbersRequest);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(2, response.getBody());
+    }
+
+    @Test
+    public void test_array_contains_zero() {
+        HighestCommonFactorService hcfService = new HighestCommonFactorService();
+        HighestCommonFactorController hcfController = new HighestCommonFactorController(hcfService);
+        NumbersRequest numbersRequest = new NumbersRequest(new int[]{2, 4, 0, 8});
+
+        ResponseEntity<?> response = hcfController.highestCommonFactor(numbersRequest);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(0, response.getBody());
+    }
+
+    @Test
+    public void test_input_array_contains_negative_numbers() {
+        HighestCommonFactorService hcfService = new HighestCommonFactorService();
+        HighestCommonFactorController hcfController = new HighestCommonFactorController(hcfService);
+        NumbersRequest numbersRequest = new NumbersRequest(new int[]{2, -4, 6, 8});
+
+        ResponseEntity<?> response = hcfController.highestCommonFactor(numbersRequest);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("Array must contain only positive numbers", response.getBody());
+    }
+
+    @Test
+    public void test_input_array_is_empty() {
+        HighestCommonFactorService hcfService = new HighestCommonFactorService();
+        HighestCommonFactorController hcfController = new HighestCommonFactorController(hcfService);
+        NumbersRequest numbersRequest = new NumbersRequest(new int[]{});
+
+        ResponseEntity<?> response = hcfController.highestCommonFactor(numbersRequest);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("Numbers array cannot be null or empty", response.getBody());
     }
 }
