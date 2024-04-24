0dd0a18b58a4e435c7682975b3cf3efbef95b7b3
marlonmilesmg <magonjomarlon@gmail.com>
2024-04-24 11:12:46 +0200

    refactored highestCommonFactor to return appropriate messages to the client

--- src/main/java/za/co/marlonmagonjo/nnw/controller/HighestCommonFactorController.java
+++ src/main/java/za/co/marlonmagonjo/nnw/controller/HighestCommonFactorController.java
@@ -1,11 +1,9 @@
 package za.co.marlonmagonjo.nnw.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import za.co.marlonmagonjo.nnw.service.HighestCommonFactorService;
 import za.co.marlonmagonjo.nnw.model.NumbersRequest;
 
@@ -20,16 +18,31 @@ public class HighestCommonFactorController {
     }
 
     /**
-     * Calculates the highest common factor (HCF) of the given numbers.
+     * Calculates the highest common factor (HCF) of an array of integers provided in the request body.
      *
-     * @param numbers the numbers for which to calculate the HCF
-     * @return the HCF as a ResponseEntity
+     * @param numbers The request body containing an array of integers.
+     * @return ResponseEntity containing the calculated HCF if successful, otherwise returns an appropriate error message.
      */
     @PostMapping("/hcf")
-    public ResponseEntity<Integer> highestCommonFactor(@RequestBody NumbersRequest numbers) {
-        log.debug("Calculating highest common factor for numbers: {}", numbers.getNumbers());
-        int hcf = hcfService.calculateHCF(numbers);
-        log.debug("Calculated HCF: {}", hcf);
-        return ResponseEntity.ok(hcf);
+    public ResponseEntity<?> highestCommonFactor(@RequestBody NumbersRequest numbers) {
+        if (numbers == null || numbers.getNumbers() == null || numbers.getNumbers().length == 0) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Numbers array cannot be null or empty");
+        }
+
+        try {
+            int hcf = hcfService.calculateHCF(numbers);
+            return ResponseEntity.ok(hcf);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(e.getMessage());
+        } catch (NullPointerException e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Numbers cannot be null");
+        }
     }
+
 }
